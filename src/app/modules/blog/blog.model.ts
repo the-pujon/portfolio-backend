4c5453280cbd980569539d8805a22ac75c98f77c
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:36:04 +0600

    blog model added

--- src/app/modules/blog/blog.model.ts
+++ src/app/modules/blog/blog.model.ts
@@ -1,1 +1,21 @@
- 
+import { Schema, model } from "mongoose";
+import { Blog } from "./blog.interface";
+
+const blogSchema = new Schema<Blog>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    image: [{ type: String }],
+    author: { type: String, required: true },
+    category: { type: String, required: true },
+    tags: [{ type: String }],
+    blogFeedback: [{ type: Schema.Types.ObjectId, ref: "BlogFeedback" }],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const BlogModel = model<Blog>("Blog", blogSchema);
+
+export default BlogModel;
