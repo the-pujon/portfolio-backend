5665f0c5e377fcc0b7d360ca0f88d37b2b894953
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 19:56:23 +0600

    project validation added

--- src/app/modules/project/project.validation.ts
+++ src/app/modules/project/project.validation.ts
@@ -1,1 +1,66 @@
- 
+import { z } from "zod";
+
+const feedbackSchema = z.object({
+  rating: z.number(),
+  email: z.string().email(),
+  feedback: z.string(),
+});
+
+const createProjectZodSchema = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    shortDescription: z.string().optional(),
+    fullDescription: z.string().optional(),
+    thumbnailImage: z.string().optional(),
+    images: z.array(z.string()).optional(),
+    videoDemo: z.string().optional(),
+    liveLink: z.string().optional(),
+    clientGithub: z.string().optional(),
+    serverGithub: z.string().optional(),
+    category: z.string().optional(),
+    projectDuration: z.string().optional(),
+    projectTeamSize: z.string().optional(),
+    projectType: z.string().optional(),
+    projectStatus: z.string().optional(),
+    projectStack: z.string().optional(),
+    projectChallenges: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    technologies: z.array(z.string()).optional(),
+    keyFeatures: z.array(z.string()).optional(),
+    challenges: z.array(z.string()).optional(),
+    solutions: z.array(z.string()).optional(),
+    feedbacks: z.array(feedbackSchema).optional(),
+  }),
+});
+
+const updateProjectZodSchema = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    shortDescription: z.string().optional(),
+    fullDescription: z.string().optional(),
+    thumbnailImage: z.string().optional(),
+    images: z.array(z.string()).optional(),
+    videoDemo: z.string().optional(),
+    liveLink: z.string().optional(),
+    clientGithub: z.string().optional(),
+    serverGithub: z.string().optional(),
+    category: z.string().optional(),
+    projectDuration: z.string().optional(),
+    projectTeamSize: z.string().optional(),
+    projectType: z.string().optional(),
+    projectStatus: z.string().optional(),
+    projectStack: z.string().optional(),
+    projectChallenges: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    technologies: z.array(z.string()).optional(),
+    keyFeatures: z.array(z.string()).optional(),
+    challenges: z.array(z.string()).optional(),
+    solutions: z.array(z.string()).optional(),
+    feedbacks: z.array(feedbackSchema).optional(),
+  }),
+});
+
+export const ProjectValidation = {
+  createProjectZodSchema,
+  updateProjectZodSchema,
+};
