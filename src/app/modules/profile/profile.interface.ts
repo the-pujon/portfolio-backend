6b06a5f4acbf837e87ce89ddd33bdd07db677cd2
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 15:24:52 +0600

    profile interface added

--- src/app/modules/profile/profile.interface.ts
+++ src/app/modules/profile/profile.interface.ts
@@ -1,1 +1,33 @@
- 
+import { Types } from "mongoose";
+
+export interface Profile {
+  name?: string;
+  email?: string;
+  designation?: string;
+  department?: string;
+  location?: {
+    city?: string;
+    country?: string;
+  };
+  heroImage?: string;
+  about?: string;
+  aboutImage?: string;
+  skills?: Types.ObjectId[];
+  projects?: Types.ObjectId[];
+  experiences?: Types.ObjectId[];
+  education?: Types.ObjectId[];
+  certifications?: Types.ObjectId[];
+  awards?: Types.ObjectId[];
+  socialMedia?: {
+    linkedin?: string;
+    twitter?: string;
+    facebook?: string;
+    instagram?: string;
+    youtube?: string;
+    github?: string;
+    leetcode?: string;
+  };
+  introduction?: string;
+  resume?: string;
+  projectCategory?: string;
+}
