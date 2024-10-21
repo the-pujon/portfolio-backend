0b70ef882ac1a6649c13164cf78111c024793c56
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 16:08:49 +0600

    project interface added

--- src/app/modules/project/project.interface.ts
+++ src/app/modules/project/project.interface.ts
@@ -1,1 +1,26 @@
- 
+export interface Project {
+  title: string;
+  description: string;
+  image: string;
+  liveLink?: string;
+  clientGithub?: string;
+  serverGithub?: string;
+  projectCategory?: string;
+  projectDuration?: string;
+  projectTeamSize?: string;
+  projectType?: string;
+  projectStatus?: string;
+  projectStack?: string;
+  projectDescription?: string;
+  projectResponsibilities?: string;
+  projectAchievements?: string;
+  projectChallenges?: string;
+  projectLearnings?: string;
+  projectTools?: string;
+  projectLinks?: string;
+  projectImages?: string[];
+  projectVideos?: string[];
+  projectDocuments?: string[];
+  projectTeam?: string[];
+  tags?: string[];
+}
