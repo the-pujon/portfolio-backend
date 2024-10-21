92d6242cec463a682a0de3e357b9e7fdd40a5e1f
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 15:39:42 +0600

    profile controller added

--- src/app/modules/profile/profile.controller.ts
+++ src/app/modules/profile/profile.controller.ts
@@ -1,1 +1,52 @@
- 
+import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { ProfileService } from "./profile.service";
+
+const createProfile = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.createProfile(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Profile created successfully",
+    data: result,
+  });
+});
+
+const getProfileById = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.getProfileById(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Profile retrieved successfully",
+    data: result,
+  });
+});
+
+const updateProfile = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.updateProfile(req.params.id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Profile updated successfully",
+    data: result,
+  });
+});
+
+const deleteProfile = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProfileService.deleteProfile(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Profile deleted successfully",
+    data: result,
+  });
+});
+
+export const ProfileController = {
+  createProfile,
+  getProfileById,
+  updateProfile,
+  deleteProfile,
+};
