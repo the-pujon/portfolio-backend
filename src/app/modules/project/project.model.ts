407e7ceecabfd05b6473e277526d9e0bd5200247
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 19:55:21 +0600

    project model added

--- src/app/modules/project/project.model.ts
+++ src/app/modules/project/project.model.ts
@@ -1,1 +1,42 @@
- 
+import { Schema, model } from "mongoose";
+import { Project } from "./project.interface";
+
+const feedbackSchema = new Schema({
+  rating: { type: Number, required: true },
+  email: { type: String, required: true },
+  feedback: { type: String, required: true },
+});
+
+const projectSchema = new Schema<Project>(
+  {
+    title: { type: String },
+    shortDescription: { type: String },
+    fullDescription: { type: String },
+    thumbnailImage: { type: String },
+    images: [{ type: String }],
+    videoDemo: { type: String },
+    liveLink: { type: String },
+    clientGithub: { type: String },
+    serverGithub: { type: String },
+    category: { type: String },
+    projectDuration: { type: String },
+    projectTeamSize: { type: String },
+    projectType: { type: String },
+    projectStatus: { type: String },
+    projectStack: { type: String },
+    projectChallenges: { type: String },
+    tags: [{ type: String }],
+    technologies: [{ type: String }],
+    keyFeatures: [{ type: String }],
+    challenges: [{ type: String }],
+    solutions: [{ type: String }],
+    feedbacks: [feedbackSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ProjectModel = model<Project>("Project", projectSchema);
+
+export default ProjectModel;
