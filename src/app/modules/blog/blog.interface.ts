8cc017fb152d818c3a4885b9d8204a01173b6d68
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:35:27 +0600

    blog interface added

--- src/app/modules/blog/blog.interface.ts
+++ src/app/modules/blog/blog.interface.ts
@@ -1,1 +1,13 @@
- 
+import { Types } from "mongoose";
+
+export interface Blog {
+  title: string;
+  content: string;
+  image: string[];
+  author: string;
+  category: string;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  blogFeedback: Types.ObjectId[];
+}
