7535f2e4eb8930065ba61200f8fe8e08842ee690
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:02:20 +0600

    contact model added

--- src/app/modules/contact/contact.model.ts
+++ src/app/modules/contact/contact.model.ts
@@ -1,1 +1,17 @@
- 
+import { Schema, model } from "mongoose";
+import { Contact } from "./contact.interface";
+
+const contactSchema = new Schema<Contact>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ContactModel = model<Contact>("Contact", contactSchema);
+
+export default ContactModel;
