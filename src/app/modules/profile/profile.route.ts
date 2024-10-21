56a9802947b702e0d32f633b77e057d59dd12313
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 15:40:43 +0600

    profile route added

--- src/app/modules/profile/profile.route.ts
+++ src/app/modules/profile/profile.route.ts
@@ -1,1 +1,21 @@
- 
+import { Router } from "express";
+import { ProfileController } from "./profile.controller";
+import { ProfileValidation } from "./profile.validation";
+import validateRequest from "../../middlewares/validateRequess";
+
+const router = Router();
+
+router.post(
+  "/create",
+  validateRequest(ProfileValidation.createProfileZodSchema),
+  ProfileController.createProfile,
+);
+router.get("/:id", ProfileController.getProfileById);
+router.patch(
+  "/:id",
+  validateRequest(ProfileValidation.updateProfileZodSchema),
+  ProfileController.updateProfile,
+);
+router.delete("/:id", ProfileController.deleteProfile);
+
+export const ProfileRoutes = router;
