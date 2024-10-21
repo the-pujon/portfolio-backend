59904dacdd09be8c8d94e668c1100b1bab2624aa
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:06:51 +0600

    education model added

--- src/app/modules/education/education.model.ts
+++ src/app/modules/education/education.model.ts
@@ -1,1 +1,19 @@
- 
+import { Schema, model } from "mongoose";
+import { Education } from "./education.interface";
+
+const educationSchema = new Schema<Education>(
+  {
+    institution: { type: String, required: true },
+    degree: { type: String, required: true },
+    fieldOfStudy: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const EducationModel = model<Education>("Education", educationSchema);
+
+export default EducationModel;
