92d35e9cfaa0e6bd31453c6d2926158ce0925a7b
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:19:41 +0600

    skill model added

--- src/app/modules/skill/skill.model.ts
+++ src/app/modules/skill/skill.model.ts
@@ -1,1 +1,17 @@
- 
+import { Schema, model } from "mongoose";
+import { Skill } from "./skill.interface";
+
+const skillSchema = new Schema<Skill>(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const SkillModel = model<Skill>("Skill", skillSchema);
+
+export default SkillModel;
