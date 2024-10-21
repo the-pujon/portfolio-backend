d9ce4579d5868417228a7813cad6d8428a1b8da3
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 15:37:23 +0600

    profile service added

--- src/app/modules/profile/profile.service.ts
+++ src/app/modules/profile/profile.service.ts
@@ -1,1 +1,45 @@
- 
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import { Profile } from "./profile.interface";
+import ProfileModel from "./profile.model";
+
+const createProfile = async (payload: Partial<Profile>): Promise<Profile> => {
+  const result = await ProfileModel.create(payload);
+  return result;
+};
+
+const getProfileById = async (id: string): Promise<Profile | null> => {
+  const result = await ProfileModel.findById(id);
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
+  }
+  return result;
+};
+
+const updateProfile = async (
+  id: string,
+  payload: Partial<Profile>,
+): Promise<Profile | null> => {
+  const result = await ProfileModel.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
+  }
+  return result;
+};
+
+const deleteProfile = async (id: string): Promise<void> => {
+  const result = await ProfileModel.findByIdAndDelete(id);
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
+  }
+};
+
+export const ProfileService = {
+  createProfile,
+  getProfileById,
+  updateProfile,
+  deleteProfile,
+};
