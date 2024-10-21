cb0acf95ff9eb1a9e54c59cd83bc3ee073ac20cc
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:16:35 +0600

    experience model added

--- src/app/modules/experience/experience.model.ts
+++ src/app/modules/experience/experience.model.ts
@@ -1,1 +1,18 @@
- 
+import { Schema, model } from "mongoose";
+import { Experience } from "./experience.interface";
+
+const experienceSchema = new Schema<Experience>(
+  {
+    companyName: { type: String, required: true },
+    position: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ExperienceModel = model<Experience>("Experience", experienceSchema);
+
+export default ExperienceModel;
