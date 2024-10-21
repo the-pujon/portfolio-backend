d740ec3bf8402a1215340d4a3a53b79807f36e15
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 19:57:58 +0600

    project route added

--- src/app/modules/project/project.route.ts
+++ src/app/modules/project/project.route.ts
@@ -1,1 +1,22 @@
- 
+import { Router } from "express";
+import { ProjectController } from "./project.controller";
+import { ProjectValidation } from "./project.validation";
+import validateRequest from "../../middlewares/validateRequess";
+
+const router = Router();
+
+router.post(
+  "/create",
+  validateRequest(ProjectValidation.createProjectZodSchema),
+  ProjectController.createProject,
+);
+router.get("/", ProjectController.getAllProjects);
+router.get("/:id", ProjectController.getProjectById);
+router.patch(
+  "/:id",
+  validateRequest(ProjectValidation.updateProjectZodSchema),
+  ProjectController.updateProject,
+);
+router.delete("/:id", ProjectController.deleteProject);
+
+export const ProjectRoutes = router;
