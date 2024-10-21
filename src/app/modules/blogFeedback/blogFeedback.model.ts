a9b5e677c89c2dd1f30514b1bb8c376a14f15d3b
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:49:10 +0600

    blog feedback model added

--- src/app/modules/blogFeedback/blogFeedback.model.ts
+++ src/app/modules/blogFeedback/blogFeedback.model.ts
@@ -1,1 +1,21 @@
- 
+import { Schema, model } from "mongoose";
+import { BlogFeedback } from "./blogFeedback.interface";
+
+const blogFeedbackSchema = new Schema<BlogFeedback>(
+  {
+    blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+    rating: { type: Number, required: true },
+    email: { type: String, required: true },
+    feedback: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const BlogFeedbackModel = model<BlogFeedback>(
+  "BlogFeedback",
+  blogFeedbackSchema,
+);
+
+export default BlogFeedbackModel;
