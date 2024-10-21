1d02862a9cacbec1fc7e431cb97dd0671bbb4179
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-21 21:48:33 +0600

    blog feedback iterface added

--- src/app/modules/blogFeedback/blogFeedback.interface.ts
+++ src/app/modules/blogFeedback/blogFeedback.interface.ts
@@ -1,1 +1,10 @@
- 
+import { Types } from "mongoose";
+
+export interface BlogFeedback {
+  blog: Types.ObjectId;
+  rating: number;
+  email: string;
+  feedback: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
