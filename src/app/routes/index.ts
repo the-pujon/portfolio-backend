e2d29451fbac9e04cdd2eb36f6008339829435b2
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-27 01:12:57 +0600

    route updated

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { ProfileRoutes } from "../modules/profile/profile.route";
 import { SkillRoutes } from "../modules/skill/skill.route";
+import { EducationRoutes } from "../modules/education/education.route";
 
 const router = Router();
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: "/skill",
     route: SkillRoutes,
+  },
+  {
+    path: "/education",
+    route: EducationRoutes,
   },
 ];
 
