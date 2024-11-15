0b4c34e414fa8c121c49032fc060b7d09c6a96ee
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-15 19:58:03 +0600

    .bat file updated

--- create_modules.bat
+++ create_modules.bat
@@ -1,18 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set modules=blog blogCategory blogFeedback contact education experience project skill projectCategory profile
+set modules=blog blogCategory blogFeedback contact education experience project skill projectCategory profile certificate
 
 for %%m in (%modules%) do (
-    mkdir src\app\modules\%%m
-    cd src\app\modules\%%m
-    echo. > %%m.controller.ts
-    echo. > %%m.interface.ts
-    echo. > %%m.model.ts
-    echo. > %%m.route.ts
-    echo. > %%m.service.ts
-    echo. > %%m.validation.ts
-    cd ..\..\..\..\
+    if not exist src\app\modules\%%m (
+        mkdir src\app\modules\%%m
+        cd src\app\modules\%%m
+        echo. > %%m.controller.ts
+        echo. > %%m.interface.ts
+        echo. > %%m.model.ts
+        echo. > %%m.route.ts
+        echo. > %%m.service.ts
+        echo. > %%m.validation.ts
+        cd ..\..\..\..\
+    ) else (
+        echo Directory src\app\modules\%%m already exists, skipping creation.
+    )
 )
 
 echo Modules created successfully.
