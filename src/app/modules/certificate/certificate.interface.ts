d76bfc28856d87444266800a2c02448b22836c0a
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-15 19:59:59 +0600

    certificate interaface added

--- src/app/modules/certificate/certificate.interface.ts
+++ src/app/modules/certificate/certificate.interface.ts
@@ -1,1 +1,11 @@
- 
+export interface Certificate {
+  id: number;
+  title: string;
+  issuedBy: string;
+  date: string;
+  image: string;
+  credentialLink?: string;
+  category: string;
+  description: string;
+  skills: string[];
+}
